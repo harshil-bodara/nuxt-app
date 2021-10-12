eb807a1aed3b561ac3f58dda64a91cf93ddefeac
harshil-bodara <developer.harshilbodara@gmail.com>
2021-10-12 16:59:21 +0530

    added styled functonality

--- components/Header.vue
+++ components/Header.vue
@@ -29,4 +29,13 @@
     align-items: center;
     width: 80%;
 }
+.nav-items a{
+    text-decoration: none;
+    color: #000;
+}
+.nav-items a:hover,
+.nav-items a:active{
+    color: cornsilk;
+}
+
 </style>
